2f227bacd3cf67fe29a1ef47979e633d8b1a0957
thijmen <thijmen.walter@hva.nl>
2025-01-08 20:24:17 +0100

    all

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <router-view class="font-mono text-xl" />
+  <router-view class="font-playwrite text-xl" />
   <h1 class="fixed right-[140px] font-bold text-lg bottom-0">Made by</h1>
   <span class="fixed right-[12px] animate-bigToSmol font-bold text-lg bottom-0"
     >WalterNoot©</span
